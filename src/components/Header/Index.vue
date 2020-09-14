49f1ff68a6199413a28e721fdbe0c53b1ad09c8c
NataliaF <natalia-321s@outlook.com>
2020-09-13 22:44:54 -0300

    Searchbar

--- src/components/Header/Index.vue
+++ src/components/Header/Index.vue
@@ -4,6 +4,10 @@
       <img class="menu-icon" src="../../assets/MenuIcon.svg" alt="Menu">
       <img class="logo" src="../../assets/youtube-logo.svg" alt="Youtube">
     </div>
+    <div class="search-bar">
+      <input type="text" placeholder="Pesquisar" class="search">
+      <button class="search"><SearchIcon :size="20"/></button>
+    </div>
     <div class="stories">
       <div class="story"></div>
       <div class="story"></div>
@@ -14,10 +18,14 @@
 </template>
 
 <script>
+import SearchIcon from "vue-material-design-icons/Magnify";
 export default {
   name: 'Header',
   props: {
     msg: String
+  },
+  components: {
+    SearchIcon,
   }
 }
 </script>
@@ -29,6 +37,7 @@ export default {
   flex-direction: row;
   background-color: var(--header);
   justify-content: space-between;
+  align-items: center;
   box-shadow: rgba(0,0,0,.1) 0 1px 0 0;
   z-index: 2;
 
@@ -48,6 +57,35 @@ export default {
       height: 24px;
       margin-left: 1rem;
       cursor: pointer;
+    }
+  }
+  .search-bar{
+    height: 30px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    input.search, button.search{
+      max-height: 30px;
+      height: 30px;
+    }
+    input{
+      max-width: 575px;
+      width: 575px;
+      background-color: var(--searchbar);
+      border-top-left-radius: 4px;
+      border-bottom-left-radius: 4px;
+      border: 1px solid var(--hover);
+      padding: .4rem;
+      color: var(--gray);
+      font-size: 16px;
+    }
+    button{
+      width: 65px;
+      background-color: var(--button);
+      border-top-right-radius: 4px;
+      border-bottom-right-radius: 4px;
+      cursor: pointer;
+      color: var(--search-icon);
     }
   }
   .stories{
